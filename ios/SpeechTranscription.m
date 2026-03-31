14e83e8283ae4e93e5c2a399e9780185479570df
Ariel Bentolila <ariel.bentolila@gmail.com>
2026-03-31 15:58:35 +0300

    feat: update bridge for two-flag attachToKeyboard and add TTS methods

--- ios/SpeechTranscription.m
+++ ios/SpeechTranscription.m
@@ -6,8 +6,10 @@
 RCT_EXTERN_METHOD(startTranscription:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(stopTranscription)
 RCT_EXTERN_METHOD(setLanguage:(NSString *)lang)
-RCT_EXTERN_METHOD(attachToKeyboard:(BOOL)enabled)
+RCT_EXTERN_METHOD(attachToKeyboard:(BOOL)textToolsEnabled speakDictateEnabled:(BOOL)speakDictateEnabled)
 RCT_EXTERN_METHOD(detachFromKeyboard)
 RCT_EXTERN_METHOD(updateFormattingState:(NSDictionary *)state)
+RCT_EXTERN_METHOD(startSpeaking:(NSString *)text fallbackLanguage:(NSString *)fallbackLanguage)
+RCT_EXTERN_METHOD(stopSpeaking)
 
 @end
